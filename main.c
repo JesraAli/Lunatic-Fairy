50d1659a1877782330cd81620d7b53e07a377af4
JesraAli <jesra.ali@gmail.com>
2023-06-22 19:35:04 +0100

    Removal of vscode folder

--- main.c
+++ main.c
@@ -22,6 +22,7 @@ SDL_Surface *surface;
 SDL_Texture *tex;
 
 int up, down, left, right;
+float x_vel, y_vel, x_pos, y_pos;
 
 void load(){
 
@@ -91,82 +92,19 @@ int main(int argc, char **argv){
     dest.h /= 6;
 
     //Sprite in centre of screen at start
-    float x_pos = (WINDOW_WIDTH - dest.w) / 2;
-    float y_pos = (WINDOW_HEIGHT - dest.h) / 2;
+     x_pos = (WINDOW_WIDTH - dest.w) / 2;
+     y_pos = (WINDOW_HEIGHT - dest.h) / 2;
+     
     //Initial sprite velocity 0 (because keyboard controls it)
-    float x_vel = 0; 
-    float y_vel = 0;
+     x_vel = 0; 
+     y_vel = 0;
 
     while (1){
-        SDL_Event event; 
-        while(SDL_PollEvent(&event)){
-            
-            switch(event.type)
-            {
-            case SDL_QUIT:
-                goto end;
-                break;
-            
-            case SDL_KEYDOWN: //meaning we've PRESSED The button on a keyboard
-            //gets key event structure, goes to its field 'keysym', then to 'scancode', retrieves the key pressed
-                switch (event.key.keysym.scancode)
-                {
-                case SDL_SCANCODE_W:
-                case SDL_SCANCODE_UP:
-                    up = 1;
-                    break;
-                case SDL_SCANCODE_A:
-                case SDL_SCANCODE_LEFT:
-                    left = 1;
-                    break;
-                case SDL_SCANCODE_S:
-                case SDL_SCANCODE_DOWN:
-                    down = 1;
-                    break;
-                case SDL_SCANCODE_D:
-                case SDL_SCANCODE_RIGHT:
-                    right = 1;
-                    break;
-                }
-                break;
-            case SDL_KEYUP:
-                switch (event.key.keysym.scancode)
-                {
-                case SDL_SCANCODE_W:
-                case SDL_SCANCODE_UP:
-                    up = 0;
-                    break;
-                case SDL_SCANCODE_A:
-                case SDL_SCANCODE_LEFT:
-                    left = 0;
-                    break;
-                case SDL_SCANCODE_S:
-                case SDL_SCANCODE_DOWN:
-                    down = 0;
-                    break;
-                case SDL_SCANCODE_D:
-                case SDL_SCANCODE_RIGHT:
-                    right = 0;
-                    break;
-                }
-                break;
-            }
 
-        }
+        prepareScene(); //Prepare Scene (Background & Clear)
 
-
-        //determine velocity of keyboard input
-        x_vel = y_vel = 0;
-        if(up && !down) y_vel = -SPEED; //if up pressed & NOT down, == negative (==up)
-        if (down && !up) y_vel = SPEED; //(positive == down)
-        if(left && !right) x_vel = -SPEED;
-        if (right && !left) x_vel = SPEED;
-
-        //update positions (divide by 60 as only  calculating 1/60th of a second)
-        x_pos += x_vel / 60;
-        y_pos += y_vel / 60;
-
-
+        doInput();
+        
         //collision detected with bounds (detect if sprite is going out of  window)
         if (x_pos <= 0) x_pos = 0; //reset positions to keep in window
         if (y_pos <= 0) y_pos = 0;
@@ -179,20 +117,91 @@ int main(int argc, char **argv){
         dest.x = (int) x_pos;
 
         
-        // clear the window
-        SDL_RenderClear(rend);
-
-        // draw the image to the window
+        // Present Scene: draw the image to the window
         SDL_RenderCopy(rend, tex, NULL, &dest);
         SDL_RenderPresent(rend);
 
-        // wait 1/60th of a second
-        SDL_Delay(1000/60);
+        SDL_Delay(1000/60); // wait 1/60th of a second
     }
-    goto end;
 
-    
-end:
+    end();
+}
+
+
+void doInput(){
+    SDL_Event event; 
+    while(SDL_PollEvent(&event)){
+
+        switch(event.type)
+        {
+        case SDL_QUIT:
+            end();
+            break;
+        
+        case SDL_KEYDOWN: //meaning we've PRESSED The button on a keyboard
+        //gets key event structure, goes to its field 'keysym', then to 'scancode', retrieves the key pressed
+            switch (event.key.keysym.scancode)
+            {
+            case SDL_SCANCODE_W:
+            case SDL_SCANCODE_UP:
+                up = 1;
+                break;
+            case SDL_SCANCODE_A:
+            case SDL_SCANCODE_LEFT:
+                left = 1;
+                break;
+            case SDL_SCANCODE_S:
+            case SDL_SCANCODE_DOWN:
+                down = 1;
+                break;
+            case SDL_SCANCODE_D:
+            case SDL_SCANCODE_RIGHT:
+                right = 1;
+                break;
+            }
+            break;
+        case SDL_KEYUP:
+            switch (event.key.keysym.scancode)
+            {
+            case SDL_SCANCODE_W:
+            case SDL_SCANCODE_UP:
+                up = 0;
+                break;
+            case SDL_SCANCODE_A:
+            case SDL_SCANCODE_LEFT:
+                left = 0;
+                break;
+            case SDL_SCANCODE_S:
+            case SDL_SCANCODE_DOWN:
+                down = 0;
+                break;
+            case SDL_SCANCODE_D:
+            case SDL_SCANCODE_RIGHT:
+                right = 0;
+                break;
+            }
+            break;
+        }
+    }
+    //determine velocity of keyboard input
+    x_vel = y_vel = 0;
+    if(up && !down) y_vel = -SPEED; //if up pressed & NOT down, == negative (==up)
+    if (down && !up) y_vel = SPEED; //(positive == down)
+    if(left && !right) x_vel = -SPEED;
+    if (right && !left) x_vel = SPEED;
+
+    //update positions (divide by 60 as only  calculating 1/60th of a second)
+    x_pos += x_vel / 60;
+    y_pos += y_vel / 60;
+
+}
+
+void prepareScene(){
+        SDL_SetRenderDrawColor(rend, 96, 128, 255, 255);
+        SDL_RenderClear(rend);
+}
+
+void end(){
     //Clean up all initialised subsystems:
     SDL_DestroyTexture(tex);
     SDL_DestroyRenderer(rend);

12739ecfd017860b71a279c37e9fc7d05b6cfd2a
JesraAli <jesra.ali@gmail.com>
2023-08-28 14:51:27 +0100

    Add main.h

---
+++ src/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void multiplayerCheck(void);
+void *serverThreadFunction(void *arg);
+void *clientThreadFunction(void *arg);
+void multiplayerCheck(void);
+
+void processPlayerNoLives(ENetPacket *packet);
+void playerNoLivesFunction(void);
+
+#endif  MAIN_H

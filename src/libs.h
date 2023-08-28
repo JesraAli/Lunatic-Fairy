e61eac8f6b23732759b47cb632382a4162327f09
JesraAli <jesra.ali@gmail.com>
2023-08-28 01:38:19 +0100

    Change path file for SDL2 headers

--- src/libs.h
+++ src/libs.h
@@ -1,7 +1,7 @@
 #ifndef libs
 #define libs
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_timer.h>
+#include <SDL.h>
+#include <SDL_timer.h>
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
 #include <stdlib.h>
